e655434a54b4f5c9db01605684cec245d611bd83
urvish sojitra <sojitraurvish0@gmail.com>
2024-09-15 21:55:10 +0530

    send json  to server

--- node 3 web servers/2/index.js
+++ node 3 web servers/2/index.js
@@ -2,12 +2,12 @@ const http = require("http");
 
 // this function create server and return it
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" ,
+  res.writeHead(200, { "Content-Type": "application/json" ,
 
   }); //write some headers in res
 
   //how to send data that we send to client and end function mean that the data that we want to send is done now send response
-  res.end('this is where i send the data'); // write a response to the client
+  res.end(JSON.stringify({ message: "Hello World" })); // write a response to the client
 });
 
 const PORT=3000
