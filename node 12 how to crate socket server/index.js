edd1db3b7fe3bd239d58e8d8241c7529d722f643
urvish <sojitraurvish@appscrip.co>
2024-10-07 00:12:34 +0530

    how to create socket io server

---
+++ node 12 how to crate socket server/index.js
@@ -0,0 +1,3 @@
+// befor image 7 we donot have our express server connected to our socket server and our server file
+
+// and for that we need to pass express as handler as request listener in to our http createServer function because express will be handling the request comming to our server and for that we have to export express from our server file 
