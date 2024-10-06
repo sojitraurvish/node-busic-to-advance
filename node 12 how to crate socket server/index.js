e7e79a16851bb6b34db230ece6a91c576eaefdd2
urvish <sojitraurvish@appscrip.co>
2024-10-07 00:20:48 +0530

    why we use http or https to create socket server

--- node 12 how to crate socket server/index.js
+++ node 12 how to crate socket server/index.js
@@ -1,3 +1,5 @@
 // befor image 7 we donot have our express server connected to our socket server and our server file
 
 // and for that we need to pass express as handler as request listener in to our http createServer function because express will be handling the request comming to our server and for that we have to export express from our server file 
+
+// watch 12th video why we use http or https to create socket server
